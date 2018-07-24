081c7bfdd2d7bdc8d8a51df7dc01a095019c3692
zozowell <zozowell@gmail.com>
2018-07-24 18:10:04 +0800

    One allocated RedisModuleString object was not properly freed with corresponding RedisModule_FreeString, it caused remarkable mem leak. This commit fix it by paring the string creating with free function call.

--- ratelimit.c
+++ ratelimit.c
@@ -115,6 +115,7 @@ int RaterLimit_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
       RedisModule_CloseKey(key);
       return RedisModule_ReplyWithError(ctx, "ERR invalid stored rater");
     }
+    RedisModule_FreeString(ctx, tat_str);
   } else if (RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_EMPTY) {
     /* If the key is not a string and is not empty it is the wrong type. */
     RedisModule_CloseKey(key);
